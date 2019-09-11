6d94860b779df684a028738c80c4970b56a5969b
Ben Stokley <JBStokley@Outlook.com>
2019-09-11 17:54:34 -0400

    Adds the developer controller endpoints.
    Adds models that will be user in the developer controller logic.
    Fixes typo in data transfer object interface.

---
+++ StorefrontApi/StorefrontApi/Controllers/DeveloperController.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StorefrontApi.DataTransfer;
+
+namespace StorefrontApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class DeveloperController : Controller
+    {
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> SearchDevelopers(DeveloperSearchParameters searchParameters)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> GetDeveloper(string developerId)
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("[action]")]
+        public async Task<IActionResult> CreateDeveloper(DeveloperDTO developer)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> UpdateDeveloper(DeveloperDTO developer)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> DeactivateDeveloper(string developerId)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ StorefrontApi/StorefrontApi/DataTransfer/DeveloperDTO.cs
@@ -0,0 +1,21 @@
+﻿using StorefrontApi.Interfaces;
+using StorefrontApi.LogicalModels;
+using System;
+
+namespace StorefrontApi.DataTransfer
+{
+    public class DeveloperDTO : IDataTransferObject<Developer>
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+        public bool Active { get; set; }
+
+        public Developer ToLogicalObject()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

3f841bb861b0233a00ee4176a3a4baa498c5e848
Ben Stokley <JBStokley@Outlook.com>
2019-09-18 17:38:20 -0400

    Adds the game controller and supporting models.

---
+++ StorefrontApi/StorefrontApi/Controllers/GameController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StorefrontApi.DataTransfer;
+
+namespace StorefrontApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GameController : Controller
+    {
+        [HttpPut]
+        [Route("[action]")]
+        public async Task<IActionResult> SearchGames(GameSearchParameters searchParameters)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> GetGame([FromQuery] string Id)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> DownloadGame([FromQuery] string Id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> CreateGame(GameDTO game, List<IFormFile> storePageFiles, List<IFormFile> gameFiles)
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("[action]")]
+        public async Task<IActionResult> UpdateGame(GameDTO game)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ StorefrontApi/StorefrontApi/LogicalModels/GameDescription.cs
@@ -0,0 +1,13 @@
+﻿namespace StorefrontApi.LogicalModels
+{
+    public class GameDescription
+    {
+        public string Value { get; }
+
+        public GameDescription(string value) => Value = value;
+
+        public override string ToString() => Value;
+
+        public static implicit operator string(GameDescription gameDescription) => gameDescription.Value;
+    }
+}

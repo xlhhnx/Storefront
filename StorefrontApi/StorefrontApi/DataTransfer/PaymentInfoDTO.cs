400429cd9760217aeed0ecc372b73cefa43aa638
Ben Stokley <JBStokley@Outlook.com>
2019-09-23 16:28:51 -0400

    Adds the payment controller and supporting models.

---
+++ StorefrontApi/StorefrontApi/DataTransfer/PaymentInfoDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StorefrontApi.DataTransfer
+{
+    public class PaymentInfoDTO
+    {
+        public string CardNumber { get; set; }
+        public string CardholderName { get; set; }
+        public string Cvn { get; set; }
+        public string ZipCode { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        // TODO: Add alternative payment methods (e.g. paypal)
+    }
+}
